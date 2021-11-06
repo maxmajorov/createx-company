3f21d227ead21e004963b6a9eb2c1d36be54e4d6
Maxim Predko <maxpredko@gmail.com>
2021-11-06 22:28:24 +0300

    feat: add portfolio-section

---
+++ src/js/components/sliders.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+
+    loop: true,
+  
+    navigation: {
+      nextEl: '.portfolio-section__next',
+      prevEl: '.portfolio-section__prev',
+    },
+  
+    
+  });
